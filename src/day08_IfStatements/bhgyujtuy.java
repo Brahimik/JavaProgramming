5e5c3b04ec3842061d9cdb76e6a6b8fbe775091e
Brahimik <ibrahim0721@icloud.com>
2022-01-28 18:44:56 +0100

    cacik dosyasi eklendi

---
+++ src/day08_IfStatements/bhgyujtuy.java
@@ -0,0 +1,13 @@
+package day08_IfStatements;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class bhgyujtuy {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("jhhgulih");
+    }
+    }
+
